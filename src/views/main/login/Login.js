104acbb66823a5b342f7c2a586ad2fd92345ba14
Nate Murray <nate@natemurray.com>
2016-07-21 17:34:45 -0700

    login styles

--- src/views/main/login/Login.js
+++ src/views/main/login/Login.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import styles from './login.module.css'
+import classnames from 'classnames'
 
 export const Login = (props) => {
   const authorizeTwitter =
     () => props.actions.users.loginWithTwitter()
+
   return (
-    <div className={styles.container}>
-      <button className={styles.button}
-              onClick={authorizeTwitter}>
-                Login with Twitter
-      </button>
+    <div className='ui middle aligned center aligned grid'>
+      <div className={classnames(['column', styles.loginColumn])}>
+
+          <div className="ui message">
+            <p>This app is a demo for the blog post <a href="#">Better Redux with Redux Modules</a></p>
+
+            <p>When you authenticate with Twitter below we will use the token to
+            show you a demo app which shows a live stream of tweets.</p>
+          </div>
+
+          <form className="ui large form">
+            <div className="ui stacked segment">
+
+              <div className="ui fluid large pink submit button"
+                   onClick={authorizeTwitter}
+              >Login with Twitter</div>
+            </div>
+          </form>
+      </div>
     </div>
   )
 }
